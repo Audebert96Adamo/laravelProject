7bc80f6a09f14e20bf0dacfd88b91c0cc35ba194
Adamo <adamo.audebert@gmail.com>
2023-04-23 01:58:27 +0200

    Admin Logout Option

--- laravel10/resources/views/admin/body/header.blade.php
+++ laravel10/resources/views/admin/body/header.blade.php
@@ -47,10 +47,10 @@
                   </button>
               </div>
 
-              @php
+              <!-- @php
               $id = Auth::user()->id;
               $adminData = App\Models\User::find($id);
-              @endphp
+              @endphp -->
 
               <div class="dropdown d-inline-block user-dropdown">
                   <button type="button" class="btn header-item waves-effect" id="page-header-user-dropdown" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
@@ -60,12 +60,12 @@
                   </button>
                   <div class="dropdown-menu dropdown-menu-end">
                       <!-- item-->
-                      <a class="dropdown-item" href="{{ route('admin.profile') }}"><i class="ri-user-line align-middle me-1"></i> Profile</a>
-                      <a class="dropdown-item" href="{{ route('change.password') }}"><i class="ri-wallet-2-line align-middle me-1"></i> Change Password</a>
+                      <!-- <a class="dropdown-item" href=""><i class="ri-user-line align-middle me-1"></i> Profile</a> -->
+                      <!-- <a class="dropdown-item" href=""><i class="ri-wallet-2-line align-middle me-1"></i> Change Password</a>
                       <a class="dropdown-item d-block" href="#"><span class="badge bg-success float-end mt-1">11</span><i class="ri-settings-2-line align-middle me-1"></i> Settings</a>
                       <a class="dropdown-item" href="#"><i class="ri-lock-unlock-line align-middle me-1"></i> Lock screen</a>
                       <div class="dropdown-divider"></div>
-
+ -->
                       <a class="dropdown-item text-danger" href="{{ route('admin.logout') }}"><i class="ri-shut-down-line align-middle me-1 text-danger"></i> Logout</a>
                   </div>
               </div>
--- laravel10/routes/web.php
+++ laravel10/routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Demo\DemoController;
+use App\Http\Controllers\AdminController;
 
 /*
 |--------------------------------------------------------------------------
