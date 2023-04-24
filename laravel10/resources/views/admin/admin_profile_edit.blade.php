34963a5c2a997fda96e297ee5f953df90cd18aa4
Adamo <adamo.audebert@gmail.com>
2023-04-24 22:56:15 +0200

    Jquery script for editSection/Profile image display

--- laravel10/resources/views/admin/admin_profile_edit.blade.php
+++ laravel10/resources/views/admin/admin_profile_edit.blade.php
@@ -1,5 +1,6 @@
 @extends('admin.admin_master')
 @section('admin')
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 
 <div class="page-content">
   <div class="container-fluid">
@@ -37,7 +38,7 @@
               <div class="row mb-3">
                 <label for="profile_image" class="col-sm-2 col-form-label">Profile Image</label>
                 <div class="col-sm-10">
-                  <input name="profile_image" class="form-control" type="file" placeholder="" id="">
+                  <input name="profile_image" class="form-control" type="file" placeholder="" id="image">
                 </div>
               </div>
               <!-- end row -->
@@ -45,7 +46,7 @@
                 <label for="profile_image" class="col-sm-2 col-form-label"></label>
 
                 <div class="col-sm-10">
-                  <img class="rounded avatar-lg" src="{{ asset('backend/assets/images/small/img-5.jpg') }}" alt="Card image cap">
+                  <img id="showImage" class="rounded avatar-lg" src="{{ asset('backend/assets/images/small/img-5.jpg') }}" alt="Card image cap">
                 </div>
               </div>
               <!-- end row -->
@@ -59,5 +60,17 @@
 
   </div>
 </div>
+
+<script type="text/javascript">
+  $(document).ready(function() {
+    $('#image').change(function(e) {
+      var reader = new FileReader();
+      reader.onload = function(e) {
+        $('#showImage').attr('src', e.target.result);
+      }
+      reader.readAsDataURL(e.target.files['0']);
+    });
+  });
+</script>
 
 @endsection
