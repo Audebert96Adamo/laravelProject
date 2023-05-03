47636f7be995b50ab347c81e6172a1a7c669de7f
Adamo <adamo.audebert@gmail.com>
2023-05-04 00:32:46 +0200

    Frontend Portfolio Show page and load data

--- laravel10/app/Http/Controllers/Home/PortfolioController.php
+++ laravel10/app/Http/Controllers/Home/PortfolioController.php
@@ -117,4 +117,10 @@ class PortfolioController extends Controller
             return redirect()->route('all.portfolio')->with($notification);
         }
     } // End Method
+
+    public function PortfolioDetails($id)
+    {
+        $portfolio = Portfolio::findOrFail($id);
+        return view('frontend.portfolio_details', compact('portfolio'));
+    } // End Method 
 }
