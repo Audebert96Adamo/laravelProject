90568f14bc40d5878be8cb31aa0fbbacb378e38f
Adamo <adamo.audebert@gmail.com>
2023-05-12 00:29:08 +0200

    using key for serial number

--- laravel10/resources/views/admin/blog_category/blog_category_all.blade.php
+++ laravel10/resources/views/admin/blog_category/blog_category_all.blade.php
@@ -37,9 +37,9 @@
 
                   <tbody>
                     @php($i = 1)
-                    @foreach($blogcategory as $item)
+                    @foreach($blogcategory as $key => $item)
                     <tr>
-                      <td>{{ $i++ }}</td>
+                      <td>{{ $key+1 }}</td>
                       <td>{{ $item->blog_category }}</td>
                       <td>
 
