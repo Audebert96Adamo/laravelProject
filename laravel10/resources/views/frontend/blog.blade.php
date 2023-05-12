c713ff553555067cc124a4c0a94878b43b6ca8c3
Adamo <adamo.audebert@gmail.com>
2023-05-12 23:48:02 +0200

    custom pagination

--- laravel10/resources/views/frontend/blog.blade.php
+++ laravel10/resources/views/frontend/blog.blade.php
@@ -67,14 +67,7 @@ All Blog | Portfolio website
 
           </div>
           <div class="pagination-wrap">
-            <!-- <nav aria-label="Page navigation example">
-              <ul class="pagination">
-                <li class="page-item"><a class="page-link" href="#"><i class="far fa-long-arrow-left"></i></a></li>
-
-              </ul>
-            </nav> -->
-
-            {{ $allblogs->links() }}
+            {{ $allblogs->links('vendor.pagination.custom') }}
           </div>
         </div>
 
